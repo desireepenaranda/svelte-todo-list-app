69e058f7cbdd87d1b6d6286a04101a64e49f9c61
Desiree Francesca Penaranda <desireefrancescapenaranda@macbook-pro.mynetworksettings.com>
2023-02-13 18:33:02 -0500

    update and delete items

--- src/components/List.svelte
+++ src/components/List.svelte
@@ -3,16 +3,23 @@
     import {items} from "../stores"
 
     import TodoApi from "../TodoApi";
+    import Item from "./Item.svelte";
     function handleNewItem(e){
+        console.log("handle new item ");
+        console.log(e);
 
     }
 
     function handleUpdate(e){
-        
+        const index = $items.findIndex(item => item.id === e.detail.id);
+        $items[index] = e.detail;
+        TodoApi.save($items);
     }
 
     function handleDelete(e){
-        
+        // in this example the object passed to thorugh the event handler is the ID
+        $items = $items.filter(item => item.id != e.detail);
+        TodoApi.save($items);
     }
 
     onMount(async() => {
@@ -24,12 +31,22 @@
 </script>
 
 <style>
+.list{
+    padding: 15px;
+}
 
+.list-status{
+    margin: 0;
+    text-align: center;
+    color: #ffffff;
+    font-weight: bold;
+    font-size: 1.1;
+}
 </style>
 
 <div class="list">
     {#each $items as item (item)}
-    {JSON.stringify(item)}
+    <Item {...item} on:update={handleUpdate} on:delete={handleDelete}/>
     {:else}
     <p class="list-status"> No Items Exist</p>
     {/each}
