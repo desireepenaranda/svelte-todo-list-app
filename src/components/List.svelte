655cd74768e483688a6fefb18df80d57f66e1799
Desiree Francesca Penaranda <desireefrancescapenaranda@macbook-pro.mynetworksettings.com>
2023-02-13 19:01:48 -0500

    add and delete item functionality and sort based on completed

--- src/components/List.svelte
+++ src/components/List.svelte
@@ -1,12 +1,21 @@
 <script>
     import {onMount} from "svelte";
-    import {items} from "../stores"
+    import {items} from "../stores";
+    import {v4 as uuidv4} from "uuid";
 
     import TodoApi from "../TodoApi";
     import Item from "./Item.svelte";
+    import NewItem from "./NewItem.svelte";
     function handleNewItem(e){
-        console.log("handle new item ");
-        console.log(e);
+        $items = [
+            {
+                id: uuidv4(),
+                text: e.detail,
+                completed: false
+            },
+            ...$items
+        ]        
+        TodoApi.save($items);
 
     }
 
@@ -20,6 +29,20 @@
         // in this example the object passed to thorugh the event handler is the ID
         $items = $items.filter(item => item.id != e.detail);
         TodoApi.save($items);
+    }
+
+    let itemsSorted = [];
+    $: {
+        itemsSorted = [...$items];
+        itemsSorted.sort((a,b) => {
+            if(a.completed && b.completed){
+                return 0;
+            }else if(a.completed){
+                return 1;
+            }else{
+                return -1;
+            }
+        })
     }
 
     onMount(async() => {
@@ -45,9 +68,11 @@
 </style>
 
 <div class="list">
-    {#each $items as item (item)}
-    <Item {...item} on:update={handleUpdate} on:delete={handleDelete}/>
+    <NewItem on:newitem={handleNewItem}/>
+    {#each itemsSorted as item (item)}
+        <Item {...item} on:update={handleUpdate} on:delete={handleDelete}/>
     {:else}
-    <p class="list-status"> No Items Exist</p>
+        <p class="list-status"> No Items Exist</p>
     {/each}
+
 </div>
